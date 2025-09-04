3fb5a50c16780a06599e4ebe8e9b0b0a7485928c
SANDHILLS\LH218-16 <jad.talbert@gmail.com>
2025-09-04 10:50:10 -0400

    Distance done

---
+++ Week 3 - Lab.py
@@ -0,0 +1,8 @@
+#Distance traveled 
+
+#velocity = 80
+#time = 2
+#distance = float(velocity)*float(time)
+#print('The velocity of {0} mph multiplied by the time {1} results to a distance of {2}'.format(velocity, time, distance))
+
+#Velocity

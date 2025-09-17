fef875c04a02123a2e42a58d68bde39efa5e7411
SANDHILLS\LH218-16 <jad.talbert@gmail.com>
2025-09-17 10:49:14 -0400

    Week 5

--- Week 5 Lab/enterGrades.py
+++ Week 5 Lab/enterGrades.py
@@ -1,4 +1,11 @@
-
+#This program allows a user to enter up to 10 grades
 
 count = 0
 numOfGrades = int(input("How many grades would you like to enter?: "))
+
+while count < numOfGrades:
+    count = count + 1 
+    grade = int(input("Enter your grade: "))
+    print = (f"You entered {grade}")
+    if (count >= numOfGrades):
+        print (f"The user enetered {numOfGrades} grades and is done.")
---
+++ Week 5 Lab/myFirstForLoop.py
@@ -0,0 +1,8 @@
+#This program loops iterations until they reach and print each value
+
+iterations = input("Enter desired amount of iterations to loop: ")
+
+print ("I will display the numbers {iterations}")
+
+for num in []:
+    print (num)

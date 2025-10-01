9a1b25ab2ec7ed8285c406da67382ad621351305
SANDHILLS\LH218-24 <jad.talbert@gmail.com>
2025-10-01 10:00:46 -0400

    Week 7

--- Week 7 Lab/kinetic_energy.py
+++ Week 7 Lab/kinetic_energy.py
@@ -1,7 +1,6 @@
-    
-def kinetic_energy():
-    m = input("Please enter your mass: ")
-    v = input("Please enter your velocity: ")
+m = input("Please enter your mass: ")
+v = input("Please enter your velocity: ")
+def kinetic_energy(m,v):
     KE = .5 * float(m) * float(v)**2
     print(f"The kenetic energy is {KE}.")
-kinetic_energy()
+kinetic_energy(m,v)
